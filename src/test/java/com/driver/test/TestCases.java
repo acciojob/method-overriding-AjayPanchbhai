b4e86bd241dcf88cf8db020f84860b0eb0ee8541
AjayPanchbhai <ajaypanchbhai111@gmail.com>
2024-08-27 14:42:54 +0530

    3 commit

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -4,5 +4,11 @@ import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
 
 public class TestCases {
-    
+    public static void main(String[] args) {
+        A a = new A();
+        System.out.println(a.meth());
+
+        a = new B();
+        System.out.println(a.meth());
+    }
 }
